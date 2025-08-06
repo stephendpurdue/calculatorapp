5780c3f3638fc52fc541a4c31649146cb6d5b4d5
Stephen Purdue <79877277+stephendpurdue@users.noreply.github.com>
2025-08-06 14:00:36 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,33 @@
+import tkinter
+
+# Sets values for each of the buttons in the interface, stored in a 2D list. 
+
+button_values = [
+    ["AC", "+/-", "%", "÷"], 
+    ["7", "8", "9", "×"], 
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "√", "="]
+]
+
+right_symbols = ["÷", "×", "-", "+", "="]
+top_symbols = ["AC", "+/-", "%"]
+
+row_count = len(button_values) 
+column_count = len(button_values[0]) 
+
+# Colours for interface.
+
+color_light_gray = "#D4D4D2"
+color_black = "#1C1C1C"
+color_dark_gray = "#505050"
+color_orange = "#FF9500"
+color_white = "#FFFFFF"
+
+# Window Setup
+
+window = tkinter.Tk()
+window.title("Calculator")
+window.resizable(False, False)
+window.mainloop()
+
