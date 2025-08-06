d3f3bd4b85fb1cdb29d04dfb8ee61564e58f211a
Stephen <stephendpurdue@gmail.com>
2025-08-06 16:59:55 +0100

    Added UI and Button functionality

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 import tkinter
 
 # Sets values for each of the buttons in the interface, stored in a 2D list. 
+# This calculator will be using a grid layout.
 
 button_values = [
     ["AC", "+/-", "%", "÷"], 
@@ -13,8 +14,8 @@ button_values = [
 right_symbols = ["÷", "×", "-", "+", "="]
 top_symbols = ["AC", "+/-", "%"]
 
-row_count = len(button_values) 
-column_count = len(button_values[0]) 
+row_count = len(button_values) #5
+column_count = len(button_values[0]) #4
 
 # Colours for interface.
 
@@ -22,7 +23,7 @@ color_light_gray = "#D4D4D2"
 color_black = "#1C1C1C"
 color_dark_gray = "#505050"
 color_orange = "#FF9500"
-color_white = "#FFFFFF"
+color_white = "white"
 
 # Window Setup
 
@@ -32,8 +33,38 @@ window.resizable(False, False)
 
 frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text="0", font=("Ariel", 45), background=color_black,
-                      foreground=color_white)
+                      foreground=color_white, anchor="e") # Will format the text label and anchor it to the right.
 
+label.grid(row=0, column=0, columnspan=column_count, sticky="we") # Text label stretches from west to east.
+
+for row in range(row_count):
+    for column in range(column_count):
+        value = button_values[row][column]
+        button = tkinter.Button(frame, text=value, font=("Arial", 30),
+                                width=column_count-1, height=1,
+                                command=lambda value=value: button_clicked(value))
+        
+        if value in top_symbols:
+            button.config(foreground=color_black, background=color_light_gray)
+        elif value in right_symbols:
+            button.config(foreground=color_white, background=color_orange)
+        else:
+            button.config(foreground=color_white, background=color_dark_gray)
+        button.grid(row=row+1, column=column)
+
+frame.pack()
+
+def button_clicked(value):
+    pass
+
+# Center the window
+window.update() # Update with new size / dimensions.
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2))
 
 window.mainloop()
 
